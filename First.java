0e2b4977797b3b43ed3506fe7ec4e0886c62511f
TMTMPST <vidoelag@gmail.com>
2023-09-04 16:30:30 +0700

    test

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First {
+
     public static void main(String[] args) {
-        System.out.println("Halo");
+        System.out.println("halo");
     }
 }
--- MyFirstJavaGithub30.java
+++ MyFirstJavaGithub30.java
@@ -2,4 +2,4 @@ public class MyFirstJavaGithub30 {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub");
     }
-}
+}   
